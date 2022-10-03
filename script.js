7bc2bd3618f86fff3e81332e8e8b8fbf092710a7
Saiki <108697998+amoebainyoface@users.noreply.github.com>
2022-10-03 21:04:47 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿let insideOfbridge = document.getElementById("inside")
+
+insideOfbridge.addEventListener("click", () => {
+    document.getElementById("textbox").innerText = "You've clicked on the inside of the moon bridge"
+})
